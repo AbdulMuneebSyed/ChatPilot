46e8ad0d97338dc40878e4b77c3b7a1c38437b37
syed abdul Muneeb <119971617+AbdulMuneebSyed@users.noreply.github.com>
2025-05-03 19:13:05 +0530

    Update users-table.tsx

--- dashboard/components/users-table.tsx
+++ dashboard/components/users-table.tsx
@@ -11,7 +11,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
     "",
- rocess.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+ process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
     ""
 );
 type Conversation = {
